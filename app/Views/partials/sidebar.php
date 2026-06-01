d25b6c373bc46c727c43a22a4f5de5ccfdabb871
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-02 00:00:41 +0700

    support sidebar section labels

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -30,12 +30,13 @@ $renderItems = static function (array $nodes, int $level = 0) use (&$renderItems
         $hasChildren = $children !== [];
         $route = trim((string) ($item['route'] ?? ''), '/');
         $active = $hasActiveChild($item);
+        $isSection = $level > 0 && $hasChildren && ($route === '' || $route === '#') && empty($item['icon']);
         $href = $route === '' || $route === '#' ? 'javascript:void(0);' : site_url($route);
-        $linkClass = $hasChildren ? 'has-arrow waves-effect' : 'waves-effect';
+        $linkClass = $hasChildren && ! $isSection ? 'has-arrow waves-effect' : 'waves-effect';
         $linkClass .= $isActiveRoute($route) ? ' active' : '';
         ?>
-        <li class="<?= $active ? 'mm-active' : '' ?>">
-            <a href="<?= $href ?>" class="<?= esc($linkClass) ?>">
+        <li class="<?= trim(($active ? 'mm-active ' : '') . ($isSection ? 'pena-menu-section' : '')) ?>">
+            <a href="<?= $isSection ? 'javascript:void(0);' : $href ?>" class="<?= esc($linkClass) ?>">
                 <?php if ($level === 0): ?>
                     <i class="bx <?= esc($item['icon'] ?: 'bx-circle') ?>"></i>
                 <?php elseif (! $hasChildren): ?>
@@ -45,7 +46,7 @@ $renderItems = static function (array $nodes, int $level = 0) use (&$renderItems
             </a>
 
             <?php if ($hasChildren): ?>
-                <ul class="sub-menu <?= $active ? 'mm-show' : '' ?>" aria-expanded="<?= $active ? 'true' : 'false' ?>">
+                <ul class="sub-menu <?= ($active || $isSection) ? 'mm-show' : '' ?>" aria-expanded="<?= ($active || $isSection) ? 'true' : 'false' ?>">
                     <?php $renderItems($children, $level + 1); ?>
                 </ul>
             <?php endif ?>
