40cf432ab5a1571d2ec39eb597cf1b5377ebbe76
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-02 15:15:02 +0700

    add sales order model

---
+++ app/Models/SalesOrderModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SalesOrderModel extends Model
+{
+    protected $table = 'sales_orders';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+    protected $allowedFields = [
+        'company_id', 'site_id', 'so_no', 'so_date', 'customer_id', 'customer_name', 'currency_code',
+        'status', 'subtotal_amount', 'tax_amount', 'discount_amount', 'total_amount',
+        'source_document_upload_id', 'notes', 'created_by', 'updated_by',
+    ];
+}
