428853c5fbb1df1c3220b89cda8532489d235839
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-02 13:56:07 +0700

    add purchase order models

---
+++ app/Models/PurchaseOrderModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PurchaseOrderModel extends Model
+{
+    protected $table = 'purchase_orders';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+    protected $allowedFields = [
+        'company_id', 'site_id', 'po_no', 'po_date', 'supplier_id', 'supplier_name', 'currency_code',
+        'status', 'subtotal_amount', 'tax_amount', 'discount_amount', 'total_amount',
+        'source_document_upload_id', 'notes', 'created_by', 'updated_by',
+    ];
+}
