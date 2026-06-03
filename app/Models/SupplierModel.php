7d113705389f8448b09b673a49337f2c68fdfc92
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-03 11:18:28 +0700

    update supplier model fields from excel dictionary

--- app/Models/SupplierModel.php
+++ app/Models/SupplierModel.php
@@ -10,6 +10,17 @@ class SupplierModel extends Model
     protected $primaryKey = 'id';
     protected $returnType = 'array';
     protected $useSoftDeletes = true;
-    protected $allowedFields = ['company_id', 'site_id', 'code', 'name', 'terms_code', 'currency_code', 'tax_number', 'address', 'phone', 'email', 'is_active', 'created_by', 'updated_by'];
     protected $useTimestamps = true;
+
+    protected $allowedFields = [
+        'company_id', 'site_id', 'code', 'name', 'terms_code', 'currency_code', 'tax_number', 'address', 'phone', 'email', 'is_active',
+        'company', 'site', 'supplier', 'supplierna', 'supplierref', 'contactnar', 'description',
+        'officeaddre', 'officecity', 'officeprovir', 'officecoun', 'officeposta', 'officeconta', 'officephon', 'officehp',
+        'mailaddres', 'mailcity', 'mailprovin', 'mailcountr', 'mailpostal', 'mailcontac', 'mailphone', 'mailhp',
+        'billingadre', 'billingcity', 'billingprovi', 'billingcoun', 'billingposta', 'billingconta', 'billingphon', 'billinghp',
+        'taxcode', 'taxnumber', 'vat', 'limitamound', 'limitqty', 'terms', 'limitdays', 'employee', 'purchasing',
+        'bank1', 'bankaccou', 'bank2', 'bankaccou2',
+        'shiptoaddr', 'shiptocity', 'shiptoprovi', 'shiptocoun', 'shiptopost', 'shiptocont', 'shiptophon', 'shiptohp',
+        'active', 'created_by', 'updated_by', 'deleted_by',
+    ];
 }
