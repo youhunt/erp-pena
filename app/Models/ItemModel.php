6c12e054be64b9b591b0a29d88b9c7c0be397abf
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-03 10:40:26 +0700

    update item model fields from excel dictionary

--- app/Models/ItemModel.php
+++ app/Models/ItemModel.php
@@ -10,22 +10,19 @@ class ItemModel extends Model
     protected $primaryKey = 'id';
     protected $returnType = 'array';
     protected $useSoftDeletes = true;
-    protected $allowedFields = [
-        'company_id',
-        'site_id',
-        'code',
-        'name',
-        'item_type',
-        'brand',
-        'stock_uom_id',
-        'sales_uom_id',
-        'purchase_uom_id',
-        'standard_cost',
-        'sales_price',
-        'shelf_life_days',
-        'is_active',
-        'created_by',
-        'updated_by',
-    ];
     protected $useTimestamps = true;
+    protected $allowedFields = [
+        'company_id', 'site_id', 'company', 'site',
+        'code', 'name',
+        'item_code', 'item_name', 'item_coded', 'item_named',
+        'shelf_life', 'stockuom', 'purchaseuom', 'sellinguom', 'stockwhs',
+        'item_price', 'purchasep', 'sellingprice', 'vat',
+        'item_length', 'item_width', 'item_heigh', 'item_diam',
+        'item_lengt', 'item_widthh', 'item_heigh_uom', 'item_diam_uom',
+        'out_length', 'out_width', 'out_height', 'out_diame',
+        'out_lengt', 'out_widthh', 'out_height_uom', 'out_diame_uom',
+        'item_group', 'item_subg', 'item_class', 'item_subc',
+        'item_type', 'item_subty', 'item_atribu',
+        'active', 'is_active', 'created_by', 'updated_by', 'deleted_by',
+    ];
 }
