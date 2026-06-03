d4d619bd85bf6ccd8235c489cb5d93b9f1db8676
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-03 11:14:58 +0700

    update customer model fields from excel dictionary

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -10,6 +10,17 @@ class CustomerModel extends Model
     protected $primaryKey = 'id';
     protected $returnType = 'array';
     protected $useSoftDeletes = true;
-    protected $allowedFields = ['company_id', 'site_id', 'code', 'name', 'terms_code', 'currency_code', 'tax_number', 'address', 'phone', 'email', 'is_active', 'created_by', 'updated_by'];
     protected $useTimestamps = true;
+
+    protected $allowedFields = [
+        'company_id', 'site_id', 'code', 'name', 'terms_code', 'currency_code', 'tax_number', 'address', 'phone', 'email', 'is_active',
+        'company', 'site', 'customer', 'customern', 'customerr', 'contactnar', 'description', 'shipwhs',
+        'officeaddre', 'officecity', 'officeprovir', 'officecount', 'officeposta', 'officeconta', 'officephon', 'officehp',
+        'taxcode', 'taxnumber', 'vat', 'limitamound', 'limitqty', 'terms', 'limitdays', 'salescode', 'salesname',
+        'bank1', 'bankaccou', 'bank2', 'bankaccou2',
+        'billingcust', 'billingtoc', 'billingaddre', 'billingcity', 'billingprovi', 'billingcoun', 'billingposta', 'billingconta', 'billingphon', 'billinghp',
+        'mailcustom', 'mailcode', 'mailaddres', 'mailcity', 'mailprovin', 'mailcountr', 'mailpostal', 'mailcontac', 'mailphone', 'mailhp',
+        'shiptocust', 'shiptocode', 'shiptoaddr', 'shiptocity', 'shiptoprovi', 'shiptocour', 'shiptopost', 'shiptocont', 'shiptophon', 'shiptohp',
+        'active', 'created_by', 'updated_by', 'deleted_by',
+    ];
 }
