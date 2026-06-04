abb12dca132a9651900d962f6a5db29bc222e0c0
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-04 14:30:32 +0700

    add purchase receipt models

---
+++ app/Models/PurchaseReceiptModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PurchaseReceiptModel extends Model
+{
+    protected $table = 'purchase_receipts';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+    protected $allowedFields = [
+        'company_id', 'site_id', 'company', 'site', 'receipt_no', 'receipt_date',
+        'purchase_order_id', 'po_no', 'supplier_id', 'supplier_code', 'supplier_name',
+        'warehouse_id', 'location_id', 'status', 'notes', 'posted_at', 'posted_by',
+        'created_by', 'updated_by',
+    ];
+}
