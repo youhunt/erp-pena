085e66fc33bbcca99291bf51e403bcf63b60f787
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-04 21:54:07 +0700

    update production work order model for work order in

--- app/Models/ProductionWorkOrderModel.php
+++ app/Models/ProductionWorkOrderModel.php
@@ -12,10 +12,12 @@ class ProductionWorkOrderModel extends Model
     protected $useSoftDeletes = true;
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'company_id', 'site_id', 'wo_code', 'wo_no', 'wo_date', 'site_code',
+        'company_id', 'site_id', 'company', 'site', 'wo_code', 'wo_no', 'wo_date', 'site_code',
         'department_code', 'warehouse_code', 'work_center_code', 'parent_item_id',
         'parent_item_code', 'parent_item_name', 'bom_id', 'routing_id',
         'batch_qty', 'wo_qty', 'std_qty_finished', 'act_qty_finished',
-        'description', 'status', 'created_by', 'updated_by',
+        'production_type', 'finished_item_id', 'finished_item_code', 'finished_item_name', 'uom_code',
+        'qty_plan', 'qty_good', 'qty_reject', 'unit_cost', 'warehouse_id', 'location_id',
+        'description', 'status', 'notes', 'posted_at', 'posted_by', 'created_by', 'updated_by',
     ];
 }
