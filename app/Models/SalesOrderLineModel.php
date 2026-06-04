c59a1669a53087d799bbecd998f8e7368aef8efa
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-04 16:54:03 +0700

    update sales order line model reservation fields

--- app/Models/SalesOrderLineModel.php
+++ app/Models/SalesOrderLineModel.php
@@ -11,7 +11,8 @@ class SalesOrderLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'sales_order_id', 'line_no', 'item_id', 'item_code', 'item_name', 'qty', 'uom_code',
-        'unit_price', 'discount_amount', 'tax_amount', 'line_total',
+        'sales_order_id', 'line_no', 'item_id', 'item_code', 'item_name',
+        'qty', 'qty_ordered', 'qty_reserved', 'qty_delivered', 'qty_outstanding',
+        'uom_code', 'unit_price', 'discount_amount', 'tax_amount', 'line_total', 'line_status',
     ];
 }
