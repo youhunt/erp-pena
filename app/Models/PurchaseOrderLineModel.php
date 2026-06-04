169e0ea96cd2962fa152c723d16ee19cbad19d90
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-04 14:04:47 +0700

    update purchase order line model receiving fields

--- app/Models/PurchaseOrderLineModel.php
+++ app/Models/PurchaseOrderLineModel.php
@@ -11,7 +11,8 @@ class PurchaseOrderLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'purchase_order_id', 'line_no', 'item_id', 'item_code', 'item_name', 'qty', 'uom_code',
-        'unit_price', 'discount_amount', 'tax_amount', 'line_total',
+        'purchase_order_id', 'line_no', 'item_id', 'item_code', 'item_name',
+        'qty', 'qty_ordered', 'qty_received', 'qty_outstanding',
+        'uom_code', 'unit_price', 'discount_amount', 'tax_amount', 'line_total', 'line_status',
     ];
 }
