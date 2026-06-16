593b2d13a3c8970e3a105e7c9f6dd7599cba66e1
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-16 17:06:24 +0700

    allow inventory movement document reversal fields

--- app/Models/InventoryMovementDocumentModel.php
+++ app/Models/InventoryMovementDocumentModel.php
@@ -14,6 +14,7 @@ class InventoryMovementDocumentModel extends Model
     protected $allowedFields = [
         'company_id', 'site_id', 'document_no', 'document_date', 'document_type', 'direction', 'status',
         'warehouse_id', 'location_id', 'total_qty', 'total_value', 'notes', 'posted_at', 'posted_by',
+        'reversed_at', 'reversed_by', 'reversal_reason', 'reversal_document_id',
         'created_by', 'updated_by',
     ];
 }
