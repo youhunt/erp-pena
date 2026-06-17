5eca74c4e9f0b8be8584ae87b23b80f57002bf7b
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-18 05:25:36 +0700

    extend purchase order line fields

--- app/Models/PurchaseOrderLineModel.php
+++ app/Models/PurchaseOrderLineModel.php
@@ -11,8 +11,11 @@ class PurchaseOrderLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'purchase_order_id', 'line_no', 'po_line', 'item_id', 'item_code', 'item_name',
+        'purchase_order_id', 'line_no', 'po_line', 'item_id', 'item_code', 'item_name', 'description',
         'qty', 'qty_ordered', 'qty_received', 'qty_outstanding',
-        'uom_code', 'unit_price', 'discount_amount', 'tax_amount', 'line_total', 'line_status',
+        'uom_code', 'unit_price', 'discount_percent', 'discount_amount',
+        'freight_amount', 'special_charge_amount', 'vat_percent', 'vat_amount',
+        'wht_percent', 'wht_amount', 'tax_amount', 'line_total', 'line_status',
+        'delivery_date', 'arrive_date',
     ];
 }
