56a402ea055bba895aaa68207b9e8a46d72a5aff
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-18 10:09:14 +0700

    limit purchase order line fields to detail data

--- app/Models/PurchaseOrderLineModel.php
+++ app/Models/PurchaseOrderLineModel.php
@@ -11,11 +11,9 @@ class PurchaseOrderLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'purchase_order_id', 'line_no', 'po_line', 'item_id', 'item_code', 'item_name', 'description',
+        'purchase_order_id', 'line_no', 'po_line',
+        'item_id', 'item_code', 'item_name', 'description',
         'qty', 'qty_ordered', 'qty_received', 'qty_outstanding',
-        'uom_code', 'unit_price', 'discount_percent', 'discount_amount',
-        'freight_amount', 'special_charge_amount', 'vat_percent', 'vat_amount',
-        'wht_percent', 'wht_amount', 'tax_amount', 'line_total', 'line_status',
-        'delivery_date', 'arrive_date',
+        'uom_code', 'unit_price', 'line_total', 'line_status',
     ];
 }
