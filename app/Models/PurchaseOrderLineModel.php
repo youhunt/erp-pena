078ede2ab5532182d3fb671ff4d5c4539e5a4d7e
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-20 10:32:15 +0700

    allow purchase order line discount and tax fields

--- app/Models/PurchaseOrderLineModel.php
+++ app/Models/PurchaseOrderLineModel.php
@@ -14,6 +14,8 @@ class PurchaseOrderLineModel extends Model
         'purchase_order_id', 'line_no', 'po_line',
         'item_id', 'item_code', 'item_name', 'description',
         'qty', 'qty_ordered', 'qty_received', 'qty_outstanding',
-        'uom_code', 'unit_price', 'line_total', 'line_status',
+        'uom_code', 'unit_price',
+        'discount_percent', 'discount_amount', 'vat_amount', 'wht_amount', 'tax_amount',
+        'line_total', 'line_status',
     ];
 }
