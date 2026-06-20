f1414f3fd8e9262f020c1adae8b31704350c4120
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-20 16:34:51 +0700

    allow purchase receipt reversal tracking fields

--- app/Models/PurchaseReceiptLineModel.php
+++ app/Models/PurchaseReceiptLineModel.php
@@ -11,8 +11,11 @@ class PurchaseReceiptLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'purchase_receipt_id', 'purchase_order_id', 'purchase_order_line_id', 'stock_movement_id', 'reversal_movement_id', 'line_no',
-        'item_id', 'item_code', 'batch_no', 'item_name', 'qty_received', 'uom_code', 'unit_cost',
+        'purchase_receipt_id', 'purchase_order_id', 'purchase_order_line_id',
+        'stock_movement_id', 'reversal_movement_id', 'line_no',
+        'item_id', 'item_code', 'batch_no', 'item_name',
+        'qty_received', 'reversed_qty', 'uom_code', 'unit_cost',
         'warehouse_id', 'location_id',
+        'reversed_at', 'reversed_by', 'reversal_reason',
     ];
 }
