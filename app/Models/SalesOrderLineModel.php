14d0278b271d9a39e8c85eaea3c3ea019ca8d316
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-21 15:13:25 +0700

    allow sales order line commercial fields

--- app/Models/SalesOrderLineModel.php
+++ app/Models/SalesOrderLineModel.php
@@ -11,8 +11,9 @@ class SalesOrderLineModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = true;
     protected $allowedFields = [
-        'sales_order_id', 'line_no', 'so_line', 'item_id', 'item_code', 'item_name',
+        'sales_order_id', 'line_no', 'so_line', 'item_id', 'item_code', 'item_name', 'description',
         'qty', 'qty_ordered', 'qty_reserved', 'qty_delivered', 'qty_outstanding',
-        'uom_code', 'unit_price', 'discount_amount', 'tax_amount', 'line_total', 'line_status',
+        'uom_code', 'unit_price', 'discount_percent', 'discount_amount', 'freight_amount',
+        'special_charge_amount', 'other_amount', 'tax_amount', 'line_total', 'line_status',
     ];
 }
