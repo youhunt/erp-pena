511196f7ea02f84911ea81a71f328a75e07682cc
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-22 15:21:08 +0700

    make sales margin audit SQL use safe invoice amount

--- database/hosting/2026-06-22_audit_sales_margin.sql
+++ database/hosting/2026-06-22_audit_sales_margin.sql
@@ -8,16 +8,16 @@ SET @invoice_no := 'SI/202606/0001';
 SELECT
     d.delivery_no,
     i.invoice_no,
-    i.total_amount AS invoice_amount,
-    ge.total_debit AS cogs_amount,
-    ROUND(i.total_amount - ge.total_debit, 2) AS gross_profit_loss,
+    COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0) AS invoice_amount,
+    COALESCE(ge.total_debit, 0) AS cogs_amount,
+    ROUND(COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0) - COALESCE(ge.total_debit, 0), 2) AS gross_profit_loss,
     CASE
-        WHEN i.total_amount = 0 THEN NULL
-        ELSE ROUND(((i.total_amount - ge.total_debit) / i.total_amount) * 100, 2)
+        WHEN COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0) = 0 THEN NULL
+        ELSE ROUND(((COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0) - COALESCE(ge.total_debit, 0)) / COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0)) * 100, 2)
     END AS gross_margin_pct,
     CASE
         WHEN ge.id IS NULL THEN 'MISSING_COGS_GL'
-        WHEN i.total_amount - ge.total_debit < 0 THEN 'LOSS_REVIEW_COST_OR_PRICE'
+        WHEN COALESCE(i.total_amount, i.grand_total, i.subtotal_amount, 0) - COALESCE(ge.total_debit, 0) < 0 THEN 'LOSS_REVIEW_COST_OR_PRICE'
         ELSE 'PROFIT_OK'
     END AS margin_status
 FROM sales_deliveries d
