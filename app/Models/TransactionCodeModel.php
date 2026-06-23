028fc17ddf3a5179dc3f439a3e7b25acfb65f3e2
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-23 23:01:49 +0700

    Refactor transaction code model for document numbering

--- app/Models/TransactionCodeModel.php
+++ app/Models/TransactionCodeModel.php
@@ -5,4 +5,18 @@ namespace App\Models;
 class TransactionCodeModel extends SetupCodeModel
 {
     protected $table = 'transaction_codes';
+    protected $allowedFields = [
+        'company_id',
+        'code',
+        'name',
+        'prefix',
+        'format',
+        'reset_period',
+        'padding',
+        'rate',
+        'description',
+        'is_active',
+        'created_by',
+        'updated_by',
+    ];
 }
