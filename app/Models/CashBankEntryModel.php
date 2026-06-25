9af3a93eb0b51de132a0c8c28d6a9101d614696e
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-25 14:14:45 +0700

    Allow rate fields on cash bank entries

--- app/Models/CashBankEntryModel.php
+++ app/Models/CashBankEntryModel.php
@@ -13,8 +13,9 @@ class CashBankEntryModel extends Model
     protected $useTimestamps = true;
     protected $allowedFields = [
         'company_id', 'site_id', 'cash_bank_account_id', 'entry_no', 'entry_date', 'entry_type',
-        'cash_bank_code', 'currency_code', 'amount', 'counter_account_no', 'reference_no',
-        'description', 'status', 'gl_entry_id', 'bank_reconciliation_id', 'reconciled_at',
-        'reconciled_by', 'posted_at', 'posted_by', 'created_by', 'updated_by',
+        'cash_bank_code', 'currency_code', 'rate_type', 'exchange_rate', 'base_currency', 'base_amount',
+        'amount', 'counter_account_no', 'reference_no', 'description', 'status', 'gl_entry_id',
+        'bank_reconciliation_id', 'reconciled_at', 'reconciled_by', 'posted_at', 'posted_by',
+        'created_by', 'updated_by',
     ];
 }
