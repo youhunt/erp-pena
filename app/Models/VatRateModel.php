3b5a8608b80baf65d78fbb0213738c5fe455d14f
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-29 22:36:24 +0700

    Extend VAT rate model for legacy VAT master fields

--- app/Models/VatRateModel.php
+++ app/Models/VatRateModel.php
@@ -2,7 +2,23 @@
 
 namespace App\Models;
 
-class VatRateModel extends SetupCodeModel
+use CodeIgniter\Model;
+
+class VatRateModel extends Model
 {
     protected $table = 'vat_rates';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+
+    protected $allowedFields = [
+        'company_id', 'site_id', 'company', 'site',
+        'vat', 'description',
+        'vatpctg', 'scpctg', 'otherpctg', 'optionalpctg',
+        'gl',
+        // legacy compatibility fields
+        'code', 'name', 'rate',
+        'is_active', 'created_by', 'updated_by',
+    ];
 }
