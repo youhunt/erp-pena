99871584ba98c24bab8847d8f950eec99a55338d
Yohan Dedik Irawan <youhunt@gmail.com>
2026-06-29 22:04:53 +0700

    Expand WHT model fields

--- app/Models/WithholdingTaxRateModel.php
+++ app/Models/WithholdingTaxRateModel.php
@@ -2,7 +2,23 @@
 
 namespace App\Models;
 
-class WithholdingTaxRateModel extends SetupCodeModel
+use CodeIgniter\Model;
+
+class WithholdingTaxRateModel extends Model
 {
     protected $table = 'wht_rates';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+
+    protected $allowedFields = [
+        'company_id', 'site_id', 'company', 'site',
+        'vat', 'description',
+        'vatpctg1', 'vatpctg2', 'vatpctg3', 'vatpctg4', 'vatpctg5',
+        'gl',
+        // legacy compatibility fields
+        'code', 'name', 'rate',
+        'is_active', 'created_by', 'updated_by',
+    ];
 }
