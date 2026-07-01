713754282e59a7fca1bba51302602ca07cc2983a
Yohan Dedik Irawan <youhunt@gmail.com>
2026-07-01 13:28:25 +0700

    Add supplier group master field

--- app/Models/SupplierModel.php
+++ app/Models/SupplierModel.php
@@ -16,7 +16,7 @@ class SupplierModel extends Model
 
     protected $allowedFields = [
         'company_id', 'site_id', 'code', 'name', 'terms_code', 'currency_code', 'tax_number', 'address', 'phone', 'email', 'is_active',
-        'company', 'site', 'supplier', 'supplierna', 'supplierref', 'contactnar', 'description',
+        'company', 'site', 'supplier', 'supplierna', 'supplierref', 'supplier_group', 'contactnar', 'description',
         'officeaddre', 'officecity', 'officeprovir', 'officecoun', 'officeposta', 'officeconta', 'officephon', 'officehp',
         'mailaddres', 'mailcity', 'mailprovin', 'mailcountr', 'mailpostal', 'mailcontac', 'mailphone', 'mailhp',
         'billingadre', 'billingcity', 'billingprovi', 'billingcoun', 'billingposta', 'billingconta', 'billingphon', 'billinghp',
